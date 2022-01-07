fca53c6362b0428e776a6661d092806e48ba9458
Howard Guan <gzhljh@gmail.com>
2022-01-07 13:03:49 -0700

    Fix favicon issue

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>DaGu Rice Noodle</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
         <meta name="description" content="DaGu Rice Noodle" />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,500;1,300&display=swap" rel="stylesheet" />
       </Head>
